1a8a6152031a8c4188ed52c6d236fc37659d7f12
alan <crwu0206@gmail.com>
2024-03-09 09:58:32 +0800

    fix

--- middleware/maintenance.global.js
+++ middleware/maintenance.global.js
@@ -1,5 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    let isMaintenance = false;
+    //是否维护中
+    const config = useRuntimeConfig()
+    let isMaintenance = config.public.MAINTENANCE_MODE;
     if (isMaintenance) {
         if (to.path.startsWith('/') && to.path !== '/maintenance') {
             return navigateTo('/maintenance');
